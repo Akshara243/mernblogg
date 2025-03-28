ac44587cc707e5ce5601c8e409ac02f86daf2452
Akshara243 <aksharans2005@gmail.com>
2025-03-28 06:50:18 +0530

    complete signin page functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cors from 'cors';
 
 
 dotenv.config();
@@ -17,12 +18,22 @@ mongoose
 });
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests only from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
+    credentials: true // Allow cookies (if needed)
+  }));
 
 app.use(express.json());
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-  });
+  });*/
+  const PORT = 5000; // Change 3000 to 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
+});
+
 
 
   app.use('/api/user', userRoutes);
