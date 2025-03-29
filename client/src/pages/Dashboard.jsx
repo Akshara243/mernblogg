13841b3ba217994895ab7561918ed4548517a106
Akshara243 <aksharans2005@gmail.com>
2025-03-29 18:58:48 +0530

    complete profile page ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ export default function Dashboard() {
   }, [location.search]);
     return(
     <div className="min-h-screen flex flex-col md:flex-row">
-        <div md:w-56>
+        <div className='md:w-56'>
           {/* sidebar */}
           <DashSidebar />
         </div>
