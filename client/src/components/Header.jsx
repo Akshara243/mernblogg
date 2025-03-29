280a948261da7fb779331d1bc215e6abbe3b1a17
Akshara243 <aksharans2005@gmail.com>
2025-03-29 10:52:42 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+
 
 export default function Header() {
     const path = useLocation().pathname;
-    const {currentUser} = useSelector(state => state.user)
+    const location = useLocation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {currentUser} = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -22,13 +28,15 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className="flex gap-2 md:order-2">
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon />
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}
+                >
+                    {theme === 'light' ? <FaSun /> : <FaMoon />} 
             </Button>
             {currentUser ? (
                 <Dropdown
                     arrowIcon={false}
-                    inlinelabel={
+                    inline
+                    label={
                         <Avatar
                             alt='user'
                             img={currentUser.profilePicture}
@@ -38,7 +46,7 @@ export default function Header() {
                 >
                     <Dropdown.Header>
                         <span className='block text-sm'>@{currentUser.username}</span>
-                        <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
                     </Dropdown.Header>
                     <Link to={'/dashboard?tab=profile'}>
                     <Dropdown.Item>Profile</Dropdown.Item>
