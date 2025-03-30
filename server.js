c67c643def241c0993576373b32b7303b4a5aa60
Akshara243 <aksharans2005@gmail.com>
2025-03-30 11:16:39 +0530

    complete profile page functionality

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
+}));
 
 // Test Route
 app.get("/", (req, res) => {
