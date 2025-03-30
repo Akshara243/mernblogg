8191c051860bc941f147802b3131c27836da3179
Akshara243 <aksharans2005@gmail.com>
2025-03-30 19:15:07 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess, } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import {HiOutlineExclamationCircle } from 'react-icons/hi';
 
@@ -142,6 +142,32 @@ const handleDeleteUser = async () => {
   }
 };
 
+const handleSignout = async () => {
+  try {
+    const res = await fetch('http://localhost:5000/api/user/signout', {
+      method: 'POST',
+      credentials: 'include',
+    });
+    const text = await res.text();
+    let data;
+    try {
+      data = JSON.parse(text); // Try parsing JSON
+    } catch {
+      data = {}; // Handle empty response
+    }
+   // const data = await res.json();
+    if(!res.ok)
+    {
+      console.log(data?.message || 'Signout failed');
+    }
+    else{
+      dispatch(signoutSuccess());
+    }
+  } catch (error) {
+    console.log('Signout error:', error.message);
+  }
+};
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -185,7 +211,7 @@ const handleDeleteUser = async () => {
         </form>
         <div className="text-red-500 flex justify-between mt-5">
           <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-          <span className='cursor-pointer'>Sign Out</span>
+          <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
         </div>
           {updateUserSuccess && (
             <Alert color='success' className='mt-5'>
